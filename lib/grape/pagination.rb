8a5d79f4d4fa25107448c2ed89e030b9a8a5b80a
Evgeniy Denisov <lifeemmulation@gmail.com>
2014-08-12 02:33:16 +0400

    Rework header parameters

--- lib/grape/pagination.rb
+++ lib/grape/pagination.rb
@@ -19,8 +19,13 @@ module Grape
             links << %(<#{url}?#{new_params.to_param}>; rel="#{k}")
           end
 
-          header 'Link', links.join(', ') unless links.empty?
-          header 'Total', ApiPagination.total_from(collection)
+          header 'X-Link', links.join(', ') unless links.empty?
+          header 'X-Total', ApiPagination.total_from(collection)
+          header "X-Total-Pages", collection.total_pages
+          header "X-Per-Page",    params[:per_page].to_s
+          header "X-Page",        collection.current_page.to_s
+          header "X-Next-Page",   collection.next_page.to_s
+          header "X-Prev-Page",   collection.prev_page.to_s
 
           return collection
         end
