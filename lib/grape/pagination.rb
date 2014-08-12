6b32997d2e21f2dc3b791d186a80dc31d55f296c
Evgeniy Denisov <lifeemmulation@gmail.com>
2014-08-12 13:14:52 +0400

    Remove Links, simplify

--- lib/grape/pagination.rb
+++ lib/grape/pagination.rb
@@ -9,23 +9,14 @@ module Grape
           }
           collection = ApiPagination.paginate(collection, options)
 
-          links = (header['Link'] || "").split(',').map(&:strip)
-          url   = request.url.sub(/\?.*$/, '')
-          pages = ApiPagination.pages_from(collection)
-
-          pages.each do |k, v|
-            old_params = Rack::Utils.parse_query(request.query_string)
-            new_params = old_params.merge('page' => v)
-            links << %(<#{url}?#{new_params.to_param}>; rel="#{k}")
-          end
-
-          header 'X-Link', links.join(', ') unless links.empty?
-          header 'X-Total', ApiPagination.total_from(collection)
-          header "X-Total-Pages", collection.total_pages
-          header "X-Per-Page",    params[:per_page].to_s
-          header "X-Page",        collection.current_page.to_s
-          header "X-Next-Page",   collection.next_page.to_s
-          header "X-Prev-Page",   collection.prev_page.to_s
+          header 'X-Pagination', { 'Total-Entries' => ApiPagination.total_from(collection),
+                                   'Total-Pages' => collection.total_pages.to_s,
+                                   'Per-Page' => options[:per_page].to_s,
+                                   'Limit-Value' => options[:per_page].to_s,
+                                   'Current-Page' => collection.current_page.to_s,
+                                   'Next-Page' => collection.next_page.to_s,
+                                   'Previous-Page' => collection.previous_page.to_s
+          }.to_json
 
           return collection
         end
